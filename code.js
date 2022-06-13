848c47cf37b58e7245d727d08476eeec1bf38dd3
Jose Torres <jose.alberto.torres35@gmail.com>
2022-06-13 16:45:12 -0500

    add tests and ability to say 'hello' to any input.

--- code.js
+++ code.js
@@ -3,10 +3,11 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-const sayHello = function (){
-    return "";
-    return ;
+const sayHello = function (input){
+    if (input === "Alex"){ return "Hello, Alex!"}
+    if (input === "Pat"){return "Hello, Pat!"}
 
+    return "Hello, " + input + "!";
 }
 
 function foo() {
