37b9ccf34ea01dab711e60f5bceb40b2ca680c3c
Jose Torres <jose.alberto.torres35@gmail.com>
2022-06-13 17:21:22 -0500

    unit tests for edge cases.

--- code.js
+++ code.js
@@ -3,12 +3,21 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-const sayHello = function (input){
-    if (input === "Alex"){ return "Hello, Alex!"}
-    if (input === "Pat"){return "Hello, Pat!"}
 
-    return "Hello, " + input + "!";
+
+const sayHello = function (input) {
+    if (input === true) {
+        return "Hello, World!";
+    } else if (input === false) {
+        return "Hello, World!";
+    }else if(input === undefined){
+        return "Hello, World!"
+    }else{
+        return "Hello, " + input + "!";
+    }
 }
+
+
 
 function foo() {
     return 2;
--- tests.js
+++ tests.js
@@ -38,7 +38,17 @@ describe('sayHello', function() {
     it('should return "Hello, Pat!', function (){
         expect(sayHello('Pat')).toBe("Hello, Pat!");
     })
+    it('should return "Hello, World!', function (){
+        expect(sayHello()).toBe("Hello, World!");
+    })
+    it('should return "Hello, World!', function (){
+        expect(sayHello(true)).toBe("Hello, World!");
+    })
+    it('should return "Hello, World!', function (){
+        expect(sayHello(false)).toBe("Hello, World!");
+    })
+    it('should return "Hello, World!', function(){
 
-
+    })
 
 });
