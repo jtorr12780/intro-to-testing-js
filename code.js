71765d404e56fa2493b0db60445642a9a201ab33
Jose Torres <jose.alberto.torres35@gmail.com>
2022-06-13 15:35:12 -0500

    test question 2 completed

--- code.js
+++ code.js
@@ -4,6 +4,8 @@ function helloWorld() {
 }
 
 const sayHello = function (){
+    return "";
+    return ;
 
 }
 
--- tests.js
+++ tests.js
@@ -26,4 +26,10 @@ describe('sayHello', function() {
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
     });
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe("string");
+    });
+
+
+
 });
